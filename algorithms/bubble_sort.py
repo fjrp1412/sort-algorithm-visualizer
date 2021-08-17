d3a295fa666c258e779615a31df2370827dd8fe7
Francisco Ruiz <fruiz.rc@gmail.com>
2021-08-17 13:30:54 -0400

    add bubble sort algorithm

---
+++ algorithms/bubble_sort.py
@@ -0,0 +1,14 @@
+import colors
+from time import sleep
+
+
+def bubble_sort(canvas, draw_data, array, speed):
+    for i in range(len(array)-1):
+        for j in range(len(array)-2):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+                colors_array = [colors.GREEN if x == j or x ==
+                                j+1 else colors.BLUE for x in range(len(array))]
+
+                sleep(speed)
+                draw_data(canvas, array, colors_array)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import tkinter as tk
 from tkinter import ttk
 import random
+import colors
+from algorithms.bubble_sort import bubble_sort
 
 CANVAS_WIDTH = 900
 CANVAS_HEIGHT = 400
@@ -13,26 +15,45 @@ root.config(bg="#fff")
 root.maxsize(1000, 900)
 
 
-def hello_world():
-    print("Hello world")
+def generate_array(canvas):
+    global array
+    array = [random.randint(1, 100) for i in range(100)]
+    draw_data(canvas, array, [colors.BLUE for i in range(len(array))])
 
 
-def draw_data(canvas):
+def draw_data(canvas, array, rectangle_colors):
     canvas.delete("all")
-    array = [random.randint(1, 100) for i in range(200)]
     max_number = max(array)
     normalize = [x/max_number for x in array]
-    spacing = 15
+    spacing = 20
     x_width = CANVAS_WIDTH / (len(array) + 1)
 
     for i, height in enumerate(normalize):
         x0 = i * x_width + spacing
         y0 = CANVAS_HEIGHT - height * 400
-        x1 = (i + 1) * x_width + 15
+        x1 = (i + 1) * x_width + spacing
         y1 = CANVAS_HEIGHT
-        canvas.create_rectangle(x0, y0, x1, y1, fill="#0ca8f6")
+        canvas.create_rectangle(x0, y0, x1, y1, fill=rectangle_colors[i])
 
     root.update_idletasks()
+
+
+def speed_time(option_list):
+    speed = option_list.get()
+    if speed == "Slow":
+        speed = 0.3
+    elif speed == "Normal":
+        speed = 0.1
+    else:
+        speed = 0.01
+    return speed
+
+
+def sort_start(canvas, array, option, speed):
+    if option == "Bubble Sort":
+        bubble_sort(canvas, draw_data, array, speed)
+
+        pass
 
 
 def run():
@@ -41,26 +62,32 @@ def run():
 
     label_algorithms = tk.Label(ui, text="Choose Algorithm: ", bg="#fff")
     label_algorithms.grid(row=0, column=0)
-    menu_algorithms = ttk.Combobox(ui, textvariable=tk.StringVar(), values=SORTS_ARRAY, state="readonly")
+    menu_algorithms = ttk.Combobox(
+        ui, textvariable=tk.StringVar(), values=SORTS_ARRAY, state="readonly")
     menu_algorithms.grid(row=0, column=1, padx=5, pady=5)
     menu_algorithms.current(0)
 
     label_time = tk.Label(ui, text="Choose sort speed: ", bg="#fff")
     label_time.grid(row=1, column=0)
+
     menu_speed = ttk.Combobox(ui, textvariable=tk.StringVar(), values=[
                               "Slow", "Normal", "Fast"], state="readonly")
     menu_speed.grid(row=1, column=1)
     menu_speed.current(0)
 
     button_array = tk.Button(ui, text="Generate Array",
-                             command=lambda: draw_data(canvas))
+                             command=lambda: generate_array(canvas))
+
     button_array.grid(row=2, column=0)
 
-    button_start = tk.Button(ui, text="Start sort")
+    button_start = tk.Button(ui, text="Start sort",
+                             command=lambda: sort_start(canvas, array, menu_algorithms.get(), speed_time(menu_speed)))
+
     button_start.grid(row=2, column=1)
 
     canvas = tk.Canvas(root, width=CANVAS_WIDTH,
                        height=CANVAS_HEIGHT, bg="#fff")
+
     canvas.grid(row=1, column=0, padx=15, pady=5)
 
     root.mainloop()
